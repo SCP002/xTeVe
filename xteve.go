58f69c8db5a0be9be0d185a108fde29646a25052
Nick Wallace <nwallace@fyberstudios.com>
2021-11-11 10:23:45 -0600

    Version bump

--- xteve.go
+++ xteve.go
@@ -39,7 +39,7 @@ var GitHub = GitHubStruct{Branch: "master", User: "Fyb3roptik", Repo: "PlexTeVe"
 const Name = "xTeVe"
 
 // Version : Version, die Build Nummer wird in der main func geparst.
-const Version = "2.3.1"
+const Version = "2.3.2.0200"
 
 // DBVersion : Datanbank Version
 const DBVersion = "2.1.0"
